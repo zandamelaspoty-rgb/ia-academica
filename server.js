93539933d1e819bef4b432f959467044846b667d
lewis-magira <lmtech093@gmail.com>
2026-05-09 18:14:29 -0700

    Permitir acesso do Blogger as ferramentas de texto

--- server.js
+++ server.js
@@ -26,6 +26,17 @@ const supabaseAdmin = createClient(
 );
 
 app.use(express.json({ limit: "1mb" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://www.lmtech93.online");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 app.use(express.static(path.join(__dirname, "public")));
 
 const upload = multer({
